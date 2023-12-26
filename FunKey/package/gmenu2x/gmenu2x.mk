fc4e29dd04ece01a671f92ebd4c8f25d9328fc66
sdhizumi <sdhizumi@live.jp>
2023-12-26 22:22:33 +0900

    Switch Gmenu2x repository to my fork

--- FunKey/package/gmenu2x/gmenu2x.mk
+++ FunKey/package/gmenu2x/gmenu2x.mk
@@ -6,7 +6,7 @@
 
 GMENU2X_VERSION = v1.0.5-funkey-s
 GMENU2X_SITE_METHOD = git
-GMENU2X_SITE = https://github.com/DrUm78/gmenu2x.git
+GMENU2X_SITE = https://github.com/sdhizumi/gmenu2x.git
 GMENU2X_LICENSE = GPL-2.0
 
 GMENU2X_DEPENDENCIES = sdl sdl_ttf sdl_gfx dejavu libpng fonts-droid
