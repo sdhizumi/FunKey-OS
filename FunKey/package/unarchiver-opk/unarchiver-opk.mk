34d836d88b1e763825c48981e43a457da401bedd
sdhizumi <sdhizumi@live.jp>
2024-01-03 14:31:51 +0900

    Update unarchiver-opk.mk

--- FunKey/package/unarchiver-opk/unarchiver-opk.mk
+++ FunKey/package/unarchiver-opk/unarchiver-opk.mk
@@ -12,9 +12,8 @@ UNARCHIVER_OPK_INSTALL = unarchiver_opk-funkey-s
 
 define UNARCHIVER_OPK_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/local/share/OPKs/Applications
-	$(HOST_DIR)/usr/bin/mksquashfs $(UNARCHIVER_OPK_PKGDIR)opk/unarchiver_opk-funkey-s \
-	$(TARGET_DIR)/usr/local/share/OPKs/Applications/unarchiver_opk-funkey-s.opk -all-root -noappend -no-exports -no-xattrs || exit 1 ; \
-	done
+	$(HOST_DIR)/usr/bin/mksquashfs $(UNARCHIVER_OPK_PKGDIR)opk/unarchiver_funkey-s \
+	$(TARGET_DIR)/usr/local/share/OPKs/Applications/unarchiver-funkey-s.opk -all-root -noappend -no-exports -no-xattrs || exit 1
 endef
 
 $(eval $(generic-package))
