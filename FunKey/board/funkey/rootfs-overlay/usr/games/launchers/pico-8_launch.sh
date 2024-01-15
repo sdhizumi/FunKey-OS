cb8f83163372ad76700aeae8984e1547d4952341
sdhizumi <sdhizumi@live.jp>
2024-01-15 22:12:17 +0900

    Remove some launch files temporary

--- FunKey/board/funkey/rootfs-overlay/usr/games/launchers/pico-8_launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Launch the process in background, record the PID into a file, wait
-# for the process to terminate and erase the recorded PID
-/usr/games/picoarch /usr/lib/libretro/fake-08_libretro.so "$1"&
-pid record $!
-wait $!
-pid erase
--- FunKey/board/funkey/rootfs-overlay/usr/games/launchers/psone_launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Launch the process in background, record the PID into a file, wait
-# for the process to terminate and erase the recorded PID
-#PicoDrive "$1"&
-/usr/games/picoarch /usr/lib/libretro/pcsx_rearmed_libretro.so "$1"&
-pid record $!
-wait $!
-pid erase
