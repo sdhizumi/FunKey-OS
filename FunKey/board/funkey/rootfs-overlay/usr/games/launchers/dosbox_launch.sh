83c8c089c03b5f63a375777361e52cfb343cbf93
sdhizumi <sdhizumi@live.jp>
2024-01-15 22:13:28 +0900

    Add launch files again

---
+++ FunKey/board/funkey/rootfs-overlay/usr/games/launchers/dosbox_launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Launch the process in background, record the PID into a file, wait
+# for the process to terminate and erase the recorded PID
+/usr/games/picoarch /usr/lib/libretro/dosbox-pure_libretro.so "$1"&
+pid record $!
+wait $!
+pid erase
